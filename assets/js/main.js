1f51adf2ffa412d79ae7350676ee46082188bac2
Foxxich <64288356+Foxxich@users.noreply.github.com>
2024-07-17 17:58:12 +0200

    added new folders, changed css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   "use strict";
 
   /**
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
     } else {
       return document.querySelector(el);
     }
-  }
+  };
 
   /**
    * Easy event listener function
@@ -20,39 +20,39 @@ document.addEventListener('DOMContentLoaded', function () {
     let selectEl = select(el, all);
     if (selectEl) {
       if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener));
+        selectEl.forEach((e) => e.addEventListener(type, listener));
       } else {
         selectEl.addEventListener(type, listener);
       }
     } else {
       console.log(`Element ${el} not found`);
     }
-  }
+  };
 
   /**
-   * Easy on scroll event listener 
+   * Easy on scroll event listener
    */
   const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener);
-  }
+    el.addEventListener("scroll", listener);
+  };
 
   /**
    * Navbar links active state on scroll
    */
-  let navbarlinks = select('#navbar .scrollto', true);
+  let navbarlinks = select("#navbar .scrollto", true);
   const navbarlinksActive = () => {
     // Check if "index" is part of the URL
-    if (!window.location.pathname.includes('index')) {
+    if (!window.location.pathname.includes("index")) {
       return;
     }
 
     let position = window.scrollY;
-    let header = select('#header');
+    let header = select("#header");
     let offset = header.offsetHeight;
 
     let activeSection = null;
 
-    navbarlinks.forEach(navbarlink => {
+    navbarlinks.forEach((navbarlink) => {
       if (!navbarlink.hash) return;
       let section = select(navbarlink.hash);
       if (!section) return;
@@ -62,91 +62,106 @@ document.addEventListener('DOMContentLoaded', function () {
       let sectionBottom = sectionTop + section.offsetHeight;
 
       // Adjusted to ensure last section gets activated
-      if (position >= sectionTop && (position < sectionBottom || (navbarlink.hash === '#contact' && position + window.innerHeight >= document.body.scrollHeight))) {
+      if (
+        position >= sectionTop &&
+        (position < sectionBottom ||
+          (navbarlink.hash === "#contact" &&
+            position + window.innerHeight >= document.body.scrollHeight))
+      ) {
         activeSection = navbarlink;
       } else {
-        navbarlink.classList.remove('active');
+        navbarlink.classList.remove("active");
       }
     });
 
     if (activeSection) {
-      activeSection.classList.add('active');
+      activeSection.classList.add("active");
     }
-  }
-  window.addEventListener('load', navbarlinksActive);
+  };
+  window.addEventListener("load", navbarlinksActive);
   onscroll(document, navbarlinksActive);
 
   /**
    * Scrolls to an element with header offset
    */
   const scrollto = (el) => {
-    let header = select('#header');
+    let header = select("#header");
     let offset = header.offsetHeight;
 
     let elementPos = select(el).offsetTop;
     window.scrollTo({
       top: elementPos - offset,
-      behavior: 'smooth'
+      behavior: "smooth",
     });
-  }
+  };
 
   /**
    * Toggle .header-scrolled class to #header when page is scrolled
    */
-  let selectHeader = select('#header');
+  let selectHeader = select("#header");
   if (selectHeader) {
     const headerScrolled = () => {
       if (window.scrollY > 100) {
-        selectHeader.classList.add('header-scrolled');
+        selectHeader.classList.add("header-scrolled");
       } else {
-        selectHeader.classList.remove('header-scrolled');
+        selectHeader.classList.remove("header-scrolled");
       }
-    }
-    window.addEventListener('load', headerScrolled);
+    };
+    window.addEventListener("load", headerScrolled);
     onscroll(document, headerScrolled);
   }
 
   /**
    * Mobile nav toggle
    */
-  on('click', '.mobile-nav-toggle', function (e) {
-    select('#navbar').classList.toggle('navbar-mobile');
-    this.classList.toggle('bi-list');
-    this.classList.toggle('bi-x');
+  on("click", ".mobile-nav-toggle", function (e) {
+    select("#navbar").classList.toggle("navbar-mobile");
+    this.classList.toggle("bi-list");
+    this.classList.toggle("bi-x");
   });
 
   /**
    * Mobile nav dropdowns activate
    */
-  on('click', '.navbar .dropdown > a', function (e) {
-    if (select('#navbar').classList.contains('navbar-mobile')) {
-      e.preventDefault();
-      this.nextElementSibling.classList.toggle('dropdown-active');
-    }
-  }, true);
+  on(
+    "click",
+    ".navbar .dropdown > a",
+    function (e) {
+      if (select("#navbar").classList.contains("navbar-mobile")) {
+        e.preventDefault();
+        this.nextElementSibling.classList.toggle("dropdown-active");
+      }
+    },
+    true
+  );
 
   /**
    * Scroll with offset on links with a class name .scrollto
    */
-  on('click', '.scrollto', function (e) {
-    if (select(this.hash)) {
-      e.preventDefault();
+  on(
+    "click",
+    ".scrollto",
+    function (e) {
+      if (select(this.hash)) {
+        e.preventDefault();
 
-      let navbar = select('#navbar');
-      if (navbar.classList.contains('navbar-mobile')) {
-        navbar.classList.remove('navbar-mobile');
-        let navbarToggle = select('.mobile-nav-toggle');
-        navbarToggle.classList.toggle('bi-list');
-        navbarToggle.classList.toggle('bi-x');
+        let navbar = select("#navbar");
+        if (navbar.classList.contains("navbar-mobile")) {
+          navbar.classList.remove("navbar-mobile");
+          let navbarToggle = select(".mobile-nav-toggle");
+          navbarToggle.classList.toggle("bi-list");
+          navbarToggle.classList.toggle("bi-x");
+        }
+        scrollto(this.hash);
       }
-      scrollto(this.hash);
-    }
-  }, true);
+    },
+    true
+  );
 
   /**
    * Scroll with offset on page load with hash links in the URL
    */
-  window.addEventListener('load', () => {
+  window.addEventListener("load", () => {
     if (window.location.hash) {
       if (select(window.location.hash)) {
         scrollto(window.location.hash);
@@ -154,55 +169,85 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 
-  var video = document.getElementById("myVideo");
-  
-  // Create an intersection observer instance
-  var observer = new IntersectionObserver(function(entries) {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        video.play();  // Play the video when it comes into view
-      } else {
-        video.pause();  // Pause the video when it goes out of view
+  /**
+   * Play/pause video on hover and intersection
+   */
+  const videos = document.querySelectorAll("video");
+
+  videos.forEach((video) => {
+    video.addEventListener("mouseenter", () => {
+      video.play();
+    });
+
+    video.addEventListener("mouseleave", () => {
+      video.pause();
+    });
+
+    video.addEventListener("ended", () => {
+      if (video.closest(":hover")) {
+        video.play();
       }
     });
-  }, { threshold: 0.5 });  // Adjust the threshold as needed
-  
-  // Observe the video element
-  observer.observe(video);
 
-  var collapseElement = document.getElementById('collapsePanel');
-  var chevronIcon = document.getElementById('chevronIcon');
-
-  if (collapseElement && chevronIcon) {
-    collapseElement.addEventListener('show.bs.collapse', function () {
-      chevronIcon.classList.remove('bi-chevron-down');
-      chevronIcon.classList.add('bi-chevron-up');
+    video.addEventListener("click", () => {
+      if (video.requestFullscreen) {
+        video.requestFullscreen();
+      } else if (video.mozRequestFullScreen) {
+        /* Firefox */
+        video.mozRequestFullScreen();
+      } else if (video.webkitRequestFullscreen) {
+        /* Chrome, Safari and Opera */
+        video.webkitRequestFullscreen();
+      } else if (video.msRequestFullscreen) {
+        /* IE/Edge */
+        video.msRequestFullscreen();
+      }
     });
+  });
 
-    collapseElement.addEventListener('hide.bs.collapse', function () {
-      chevronIcon.classList.remove('bi-chevron-up');
-      chevronIcon.classList.add('bi-chevron-down');
-    });
-  }
+  // Create an intersection observer instance
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        const video = entry.target;
+        if (entry.isIntersecting) {
+          video.play();
+        } else {
+          video.pause();
+        }
+      });
+    },
+    { threshold: 0.5 }
+  );
+
+  // Observe each video element
+  videos.forEach((video) => {
+    observer.observe(video);
+  });
 
   /**
    * Intersection Observer for animations on scroll
    */
   const observerOptions = {
-    threshold: 0.1
+    threshold: 0.1,
   };
 
   const observerCallback = (entries, visibilityObserver) => {
-    entries.forEach(entry => {
+    entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.add('visible');
+        entry.target.classList.add("visible");
         visibilityObserver.unobserve(entry.target);
       }
     });
   };
 
-  const visibilityObserver = new IntersectionObserver(observerCallback, observerOptions);
+  const visibilityObserver = new IntersectionObserver(
+    observerCallback,
+    observerOptions
+  );
 
-  const elementsToObserve = document.querySelectorAll('.image-item, .section-title, .social-links, .portfolio');
-  elementsToObserve.forEach(element => visibilityObserver.observe(element));
+  const elementsToObserve = document.querySelectorAll(
+    ".image-item, .section-title, .social-links, .portfolio"
+  );
+  elementsToObserve.forEach((element) => visibilityObserver.observe(element));
 });
